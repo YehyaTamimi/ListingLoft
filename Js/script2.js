9b4590bb80d8440fa750b3abe395381d9eecc063
YehyaTamimi <tamimiyehya@gmail.com>
2024-03-28 14:50:44 +0200

    search script file

--- Js/script2.js
+++ Js/script2.js
@@ -1,5 +1,9 @@
+import json from "./proporties.js";
+
+
 let lastOpenContainer;
 document.addEventListener('DOMContentLoaded', () => {
+    loadcontent();
     document.querySelector(".price-button").addEventListener("click", () => {
         viewRange("price")
     });
@@ -12,8 +16,74 @@ document.addEventListener('DOMContentLoaded', () => {
         viewRange("rooms")
     });
 
+    document.querySelector(".home").addEventListener("click", () => {
+        window.location.href = "/html/index.html";
+    })
+
 })
 
+const requestListings = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '41602150d1msh5c4982690c63891p129b85jsn781a18c8417a',
+            'X-RapidAPI-Host': 'us-real-estate-listings.p.rapidapi.com'
+        }
+    };
+
+    const params = {
+        location: 'Metairie, LA',
+        offset: '0',
+        limit: '100',
+    }
+
+    const queryString = new URLSearchParams(params).toString()
+    let url = `https://us-real-estate-listings.p.rapidapi.com/for-sale?${queryString}`
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            return new Error("Error loading movies");
+        })
+        .then(loadcontent)
+        .catch(err => console.error('error:' + err));
+
+}
+
+const loadcontent = () => {
+    let houses = json["listings"];
+    let container = document.querySelector(".cards-container");
+
+    houses.forEach((house) => {
+        let card = createCard(house);
+        container.appendChild(card);
+    });
+}
+
+const createCard = (house) => {
+    let desc = house["description"];
+    let baths = desc["baths_full"];
+    let beds = desc["beds"];
+    let size = desc["sqft"];
+    let location = house["location"];
+    let city = location["address"]["city"];
+    let state = location["address"]["state"];
+    let street = location["address"]["street_name"];
+    let price = house["list_price"].toLocaleString();
+    let image = house["primary_photo"]["href"];
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<img src="${image}" alt="" class="card-img">
+                          <div class="info">
+                                <p class="price">$${price}</p>
+                                <p class="small-info">${beds} bed | ${baths} bath | ${size} sqrt | ${street}, ${city}, ${state}</p>
+                          </div>`
+    return card;
+
+}
 const viewRange = (element) => {
     let button = document.querySelector(`.${element}-button`);
     console.log(button)
@@ -31,7 +101,7 @@ const viewRange = (element) => {
         icon.classList.remove("fa-caret-up")
         icon.classList.add("fa-caret-down")
         let container = document.querySelector(`.${element}-container`);
-            container.removeChild(container.lastElementChild);
+        container.removeChild(container.lastElementChild);
     }
 }
 
@@ -90,7 +160,7 @@ const createRoomsElement = () => {
     let bedroomNumber = document.querySelector('.bedrooms-number');
     let bedroomButtons = bedroomNumber.querySelectorAll('button');
     bedroomButtons.forEach((button) => {
-        button.addEventListener('click',  () => {
+        button.addEventListener('click', () => {
             console.log(button.textContent[0]);
         });
     });
@@ -101,7 +171,7 @@ const checkOpenContainers = () => {
 
     containers.forEach((container) => {
         let parent = document.querySelector(`.${container}-container`);
-        if(parent.lastElementChild.tagName == "DIV"){
+        if (parent.lastElementChild.tagName == "DIV") {
             let button = document.querySelector(`.${container}-button`);
             let icon = button.lastElementChild;
             icon.classList.remove("fa-caret-up")
