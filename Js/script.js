ab90d86695fb4f72ad879ff67f453415e902f919
YehyaTamimi <tamimiyehya@gmail.com>
2024-03-31 10:59:55 +0200

    moved repeated code into a module

--- Js/script.js
+++ Js/script.js
@@ -1,95 +1,47 @@
-
+//import hardcoded data from json file
 import json from "./newListings.js";
+import { createCard } from "./createHouseCard.js";
+import { requestListings } from "./requestAPI.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    startswiper();
-    loadcontent();
-    document.querySelector(".discover").addEventListener('click', goToSearchPage)
-    document.querySelector(".search").addEventListener('click', goToSearchPage);;
-    document.querySelector(".search-input").addEventListener("keypress", (e) => {
-      if (e.key === 'Enter') {
-          goToSearchPage();
-      }});
+  startswiper();
+  loadcontent();
+  document.querySelector(".discover").addEventListener('click', goToSearchPage)
+  document.querySelector(".search").addEventListener('click', goToSearchPage);;
+  document.querySelector(".search-input").addEventListener("keypress", handleKeyPress);
 })
 
-const requestListings = () => {
-  const options = {
-      method: 'GET',
-      headers: {
-          'X-RapidAPI-Key': '41602150d1msh5c4982690c63891p129b85jsn781a18c8417a',
-          'X-RapidAPI-Host': 'us-real-estate-listings.p.rapidapi.com'
-      }
-  };
 
-  const params = {
-      location: 'Metairie, LA',
-      offset: '0',
-      limit: '100',
-      sort: 'newest'
-  }
-
-  const queryString = new URLSearchParams(params).toString()
-  let url = `https://us-real-estate-listings.p.rapidapi.com/for-sale?${queryString}`
-
-  fetch(url, options)
-      .then(response => {
-          if (response.ok) {
-              return response.json();
-          }
-          return new Error("Error loading movies");
-      })
-      .then(loadcontent)
-      .catch(err => console.error('error:' + err));
-
-}
-
+//load content from json file
 const loadcontent = () => {
   let houses = json["listings"];
   let container = document.querySelector(".card-container");
   let count = 10;
 
   houses.forEach((house) => {
-    if(count === 0) return;
+    if (count === 0) return;
     let card = createCard(house);
     container.appendChild(card);
     count--;
   });
 }
 
-const createCard = (house) => {
-      let desc = house["description"];
-      let baths = desc["baths_full"];
-      let beds = desc["beds"];
-      let size = desc["sqft"];
-      let location = house["location"];
-      let city = location["address"]["city"];
-      let state = location["address"]["state"];
-      let street = location["address"]["street_name"];
-      let price = house["list_price"].toLocaleString();
-      let image = house["primary_photo"]["href"];
-
-      let card = document.createElement("div");
-      card.classList.add("card");
-      card.classList.add("swiper-slide");
-      card.innerHTML = `<img src="${image}" alt="" class="card-img">
-                        <button class="add-favorite"><i class="fa-regular fa-heart"></i></button>
-                        <div class="info">
-                              <p class="price">$${price}</p>
-                              <p class="small-info">${beds} bed | ${baths} bath | ${size} sqrt | ${street}, ${city}, ${state}</p>
-                        </div>`
-      return card;
-  
+// handle Enter key press for search
+const handleKeyPress = (e) => {
+  if (e.key === 'Enter') {
+    goToSearchPage();
+  }
 }
 
 const goToSearchPage = () => {
   window.location.href = "search.html";
 }
 
-const startswiper = () =>{
+const startswiper = () => {
   const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
+    spaceBetween: 30,
+    freeMode: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -98,6 +50,6 @@ const startswiper = () =>{
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-});
+  });
 }
 
