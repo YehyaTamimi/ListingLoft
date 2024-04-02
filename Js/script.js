c6b8e786f64e0fd6e9eab6f018bc77e515bb5975
YehyaTamimi <tamimiyehya@gmail.com>
2024-04-02 09:45:54 +0200

    save user search history
    changes to be commited

--- Js/script.js
+++ Js/script.js
@@ -2,19 +2,39 @@
 import json from "./newListings.js";
 import { createCard } from "./createHouseCard.js";
 import { requestListings } from "./requestAPI.js";
+import { viewSearchHistory, addToSearchHistory, closeHistory } from "./history.js";
 
+let searchArr = [];
 document.addEventListener("DOMContentLoaded", () => {
   startswiper();
   loadcontent();
-  document.querySelector(".discover").addEventListener('click', goToSearchPage)
-  document.querySelector(".search").addEventListener('click', goToSearchPage);;
-  document.querySelector(".search-input").addEventListener("keypress", handleKeyPress);
+
+  if (localStorage.getItem('history') !== null) {
+    searchArr = JSON.parse(localStorage.getItem('history'));
+}
+
+  document.querySelector(".discover").addEventListener('click', goToSearchPage);
+  document.querySelector(".search").addEventListener('click', goToSearchPage);
+
+  const input = document.querySelector(".search-input");
+  input.addEventListener("keypress", handleKeyPress);
+  input.addEventListener("click", () => {
+    viewSearchHistory(searchArr)}
+    );
+
+    document.addEventListener('click', (e) => {
+      if (!e.target.closest('.search-input')) {
+          closeHistory();
+      }
+    });
+
 })
 
 
 //load content from json file
 const loadcontent = () => {
   let houses = json["listings"];
+  let container = document.querySelector(".card-container");
   let container = document.querySelector(".card-container");
   let count = 10;
 
@@ -34,8 +54,12 @@ const handleKeyPress = (e) => {
 }
 
 const goToSearchPage = () => {
+  const input = document.querySelector(".search-input");
+  sessionStorage.setItem("query", input.value.trim());
+  addToSearchHistory(input.value.trim(), searchArr);
   window.location.href = "search.html";
 }
+
 
 const startswiper = () => {
   const swiper = new Swiper('.swiper', {
