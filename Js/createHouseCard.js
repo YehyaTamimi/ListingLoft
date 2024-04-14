047026ed8d6e3c68d44f51443e515a067eaf7cf7
YehyaTamimi <tamimiyehya@gmail.com>
2024-04-14 13:30:42 +0200

    skip hosues with missing information

--- Js/createHouseCard.js
+++ Js/createHouseCard.js
@@ -1,31 +1,46 @@
 
 export const createCard = (house) => {
-    const { description, location, list_price, primary_photo, property_id } = house;
-    const { baths_full: baths, beds, sqft: size } = description;
-    const { address: { city, state, street_name: street } } = location;
+    let street1, size1, baths1, beds1, city1, state1;
+    const { description, location, list_price, photos, property_id } = house;
+    if (window.location.pathname.includes("favorites.html")) {
+        const { baths, beds, sqft } = description;
+        const { address: { city, state, street_name } } = location;
+        size1 = sqft;
+        street1 = street_name;
+        baths1 = baths;
+        beds1 = beds;
+        city1 = city;
+        state1 = state;
+    } else {
+        const { baths_full: baths, beds, sqft: size } = description;
+        const { address: { city, state, street_name: street } } = location;
+        size1 = size;
+        street1 = street;
+        baths1 = baths;
+        beds1 = beds;
+        city1 = city;
+        state1 = state;
+    }
+
+    if (!photos || !photos[0].href) {
+        return;
+    }
+
+    if (!description || !location || !list_price || !property_id) {
+        return;
+    }
+
+    if (!baths1 || !beds1 || !size1 || !city1 || !state1 || !street1 || !price) {
+        return;
+    }
+
     const price = list_price.toLocaleString();
-
- if (!primary_photo || !primary_photo.href) {
-    console.log("No image available for this property.");
-    return; 
-}
-
-const image = primary_photo["href"];
-
-if (!description || !location || !list_price || !property_id) {
-    console.log("One or more required properties are missing from the JSON data.");
-    return; 
-}
-
-if (!baths || !beds || !size || !city || !state || !street || !price) {
-    console.log("One or more extracted values are empty.");
-    return; 
-}
+    const image = photos[0].href;
 
 
 
     const container = document.querySelector(".cards-container");
-  
+
 
     let card = document.createElement("div");
     card.classList.add("card");
@@ -35,48 +50,33 @@ if (!baths || !beds || !size || !city || !state || !street || !price) {
                           <button class="add-favorite"><i class="fa-regular fa-heart"></i></button>
                           <div class="info">
                                 <p class="price">$${price}</p>
-                                <p class="small-info">${beds} bed | ${baths} bath | ${size} sqrt | ${street}, ${city}, ${state}</p>
+                                <p class="small-info">${beds1} bed | ${baths1} bath | ${size1} sqrt | ${street1}, ${city1}, ${state1}</p>
                           </div>`
 
     const button = card.querySelector(".add-favorite");
-    button.addEventListener("click", ()=>{
+    button.addEventListener("click", () => {
         const icon = button.querySelector("i");
-        if(icon.classList[2] == "fa-solid"){
+        if (icon.classList[2] == "fa-solid") {
             icon.classList.remove("fa-solid");
-        }else{
+        } else {
             icon.classList.add("fa-solid");
         }
         addToFavorites(`h${property_id}`)
     })
     container.appendChild(card);
-  }
+}
 
 const addToFavorites = (id) => {
     let arr = [];
-    if(localStorage.getItem("favorite") !== null){
+    if (localStorage.getItem("favorite") !== null) {
         arr = JSON.parse(localStorage.getItem("favorite"));
-      }
-    if (arr.includes(id)){
+    }
+    if (arr.includes(id)) {
         let index = arr.indexOf(id);
         let removed = arr.splice(index, 1);
-
-        // if(window.location.pathname.includes("favorites.html")){
-        //     removeFavorite(removed);
-        // }
-    }else{
+    } else {
         arr.push(id);
     }
     localStorage.setItem("favorite", JSON.stringify(arr));
     console.log(arr)
 }
-
-// const removeFavorite = (removed)=>{
-//     document.querySelector(`.${removed.toString()}`).remove();
-
-//     const container = document.querySelector(".cards-container");
-//     if(container.childNodes.length === 0){
-//         container.innerHTML = "<p class=empty>No Favorites Found In Your List<p>";
-//     }
-
-// }
-
