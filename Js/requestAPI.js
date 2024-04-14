72375e865027dce1f08274c743ab82d968d04e23
YehyaTamimi <tamimiyehya@gmail.com>
2024-04-14 13:32:59 +0200

    check query origin

--- Js/requestAPI.js
+++ Js/requestAPI.js
@@ -1,7 +1,11 @@
-export const requestListings = (query, type, callback) => {
-  addLoading();
-  let params;
-  let apiKey = "41602150d1msh5c4982690c63891p129b85jsn781a18c8417a";
+
+
+export const requestListings = (query, filter, callback, favorite = false, counter = 0) => {
+  if (counter === 0) {
+    addLoading();
+  }
+
+  let apiKey = "90eed311b6msh738eaffb019994fp1778b8jsn03b51b3ab3fd";
   const options = {
     method: 'GET',
     headers: {
@@ -10,30 +14,25 @@ export const requestListings = (query, type, callback) => {
     }
   };
 
-  if (sessionStorage.getItem('params') !== null && query !== "") {
-    params = JSON.parse(localStorage.getItem("params"));
-  } else {
+  let url;
+  let params;
+
+  if (favorite) {
     params = {
-      location: 'california',
-      offset: '0',
-      limit: '200',
-      sort: 'newest'
-    }
+      id: query
+    };
+
+  } else {
+    params = checkQuery(query, filter);
+    console.log(params);
   }
 
-  checkQuery(type, query, params);
-
-  console.log(params);
-
-  const queryString = new URLSearchParams(params).toString()
-  let url = `https://us-real-estate-listings.p.rapidapi.com/for-sale?${queryString}`
-
-
+  const queryString = new URLSearchParams(params).toString();
+  url = favorite ? `https://us-real-estate-listings.p.rapidapi.com/v2/property?${queryString}` : `https://us-real-estate-listings.p.rapidapi.com/for-sale?${queryString}`;
 
   fetch(url, options)
     .then(response => {
       if (response.ok) {
-        console.log("ok");
         removeLoading()
         return response.json();
       }
@@ -59,64 +58,34 @@ const removeLoading = () => {
   const body = document.querySelector("body");
   body.classList.add("grayout");
   const contain = document.querySelector(".loading");
-  body.removeChild(contain);
+  if (contain) {
+    body.removeChild(contain);
+  }
 
 }
 
-const checkQuery = (type, query, params) => {
-  switch (type) {
-    case "rooms":
-      if (query.beds_min !== "0") {
-        params.bed_min = query.beds_min;
-        params.beds_max = query.beds_max;
-      }
-      if (query.baths_min !== "0") {
-        params.baths_min = query.baths_min;
-        params.baths_max = query.baths_max;
-      }
-      break;
-    case "price":
-      params.price_min = query.price_min;
-      params.price_max = query.price_max;
-      break;
-    case "size":
-      params.size_min = query.size_min;
-      params.size_max = query.size_max;
-      break;
-    case "reset-rooms":
-      delete params.bed_min;
-      delete params.beds_max;
-      delete params.baths_min;
-      delete params.baths_max;
-      break;
-    case "reset-price":
-      delete params.price_min;
-      delete params.price_max;
-      break;
-    case "reset-size":
-      delete params.size_min;
-      delete params.size_max;
-      break;
-    default:
-      if (query !== "") {
-        query = query.toLowerCase();
-        //check if query only contains numbers
-        let isnum = /^\d+$/.test(query);
-        if (isnum === false) {
-          params.location = query;
-        } else {
-          if (query.length >= 5) {
-            params.price_max = query;
-          } else {
-            params.home_size_max = query;
-          }
-        }
-      }
-      break;
+const checkQuery = (query, filter) => {
+  let params = {
+    location: 'california',
+    offset: '0',
+    limit: '200',
+    sort: 'newest'
   }
 
-  if (type !== "") {
-    localStorage.setItem("params", JSON.stringify(params));
+  if (query !== "") {
+    query = query.toLowerCase();
+    //check if query only contains numbers
+    let isnum = /^\d+$/.test(query);
+    if (isnum === false) {
+      params.location = query;
+    } else {
+      if (query.length >= 5) {
+        params.price_max = query;
+      } else {
+        params.home_size_max = query;
+      }
+    }
   }
+
+  return { ...params, ...filter };
 }
-
